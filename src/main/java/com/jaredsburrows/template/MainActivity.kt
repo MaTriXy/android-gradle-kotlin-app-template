d23c813539770ea461ed80d784549213c7c1067f
Jared Burrows <jaredsburrows@gmail.com>
2019-03-17 21:46:24 -0700

    bring project up to date

--- src/main/java/com/jaredsburrows/template/MainActivity.kt
+++ src/main/java/com/jaredsburrows/template/MainActivity.kt
@@ -1,17 +1,13 @@
-package burrows.apps.example.template.activity
+package com.jaredsburrows.template
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import burrows.apps.example.template.R
 
-/**
- * @author <a href="mailto:jaredsburrows@gmail.com">Jared Burrows</a>
- */
 class MainActivity : AppCompatActivity() {
 
     override
     fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        this.setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main)
     }
 }
